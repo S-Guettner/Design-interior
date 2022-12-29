3456c6e9e25f6c9b05a38c9d8a51e1c89094df87
S-Guettner-Dev <sven_guettner@protonmail.com>
2022-12-29 15:56:56 +0100

    input

---
+++ assets/js/app.js
@@ -0,0 +1,10 @@
+
+
+let nameFunction = () =>{
+    let nameInput = document.getElementById("name").value;
+    let emailInput = document.getElementById("email").value;
+    let messageInput = document.getElementById("message").value;
+    console.log(nameInput);
+    console.log(emailInput);
+    console.log(messageInput);
+}
